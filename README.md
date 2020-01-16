f66060e02c6bb6ecc4c4590cab57b3b148bf0cb7
Bruno Marques (allakazan) <brunorizzardi80014@hotmail.com>
2020-01-16 17:09:45 -0300

    first commit <3

---
+++ README.md
@@ -0,0 +1,20 @@
+
+## About
+
+Crud básico de contatos com Laravel
+
+## Instructions
+
+### Instalação
+
+`composer install`
+
+`npm i`
+
+`php artisan migrate`
+
+Dados do banco no .env
+
+### Run
+
+`php artisan serve`
---
+++ resources/views/base.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contatos</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <div class="container">
+    @yield('main')
+  </div>
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
